2209be3166adf84601599022386ae704bdf21141
NikaMelashvili <melashvilinikusha63@gmail.com>
2024-08-21 01:15:57 +0400

    login implemented

--- Registration-Front/src/app/components/login/login.component.ts
+++ Registration-Front/src/app/components/login/login.component.ts
@@ -1,38 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormGroup, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  ReactiveFormsModule,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { Login } from '../../common/login';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
-  template: `<div class="container-login">
-    <form [formGroup]="loginFormGroup" (ngSubmit)="login()">
-      <div class="mb-3">
-        <label for="email" class="form-label">Email address</label>
-        <input
-          type="email"
-          class="form-control"
-          id="email"
-          formControlName="email"
-          placeholder="name@example.com"
-        />
-      </div>
-      <div class="mb-3">
-        <label for="password" class="form-label">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="password"
-          formControlName="password"
-          placeholder="Password"
-        />
-      </div>
-      <div class="mb-3">
-        <button type="submit" class="btn btn-primary mb-3">Submit</button>
-      </div>
-    </form>
-  </div> `,
+  template: `
+    <div class="container-sign-up">
+      <form [formGroup]="loginFormGroup" (ngSubmit)="login()">
+        <div class="mb-3">
+          <label for="email" class="form-label">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            formControlName="email"
+            placeholder="name@example.com"
+          />
+        </div>
+        <div class="mb-3">
+          <label for="password" class="form-label">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            formControlName="password"
+            placeholder="Password"
+          />
+        </div>
+        <div class="mb-3">
+          <button type="submit" class="btn btn-primary mb-3">Submit</button>
+        </div>
+      </form>
+    </div>
+  `,
   styles: [
     `
       .container-login {
@@ -44,15 +54,39 @@ import { FormGroup, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 export class LoginComponent implements OnInit {
   loginFormGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  userLogin: Login | null = null;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.loginFormGroup = this.formBuilder.group({
-      email: [''],
-      password: [''],
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
     });
   }
+
   login() {
-    throw new Error('Method not implemented.');
+    if (this.loginFormGroup.valid) {
+      const { email, password } = this.loginFormGroup.value;
+      this.userLogin = new Login(email, password);
+
+      this.authService.userLogin(this.userLogin).subscribe({
+        next: (response) => {
+          console.log('Login successful, token:', response.token);
+        },
+        error: (error) => {
+          console.error('Login failed:', error);
+        },
+      });
+    }
   }
 }
--- Registration-Front/src/app/services/auth.service.ts
+++ Registration-Front/src/app/services/auth.service.ts
@@ -2,16 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { SignUp } from '../common/sign-up';
 import { Observable } from 'rxjs';
+import { Login } from '../common/login';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private signUpUrl = 'http://127.0.0.1:8080/rest/authentication/register';
+  private loginUrl = 'http://127.0.0.1:8080/rest/authentication/authenticate';
 
   constructor(private http: HttpClient) {}
 
   userRegister(user: SignUp): Observable<{ token: string }> {
     return this.http.post<{ token: string }>(this.signUpUrl, user);
+  }
+
+  userLogin(user: Login): Observable<{ token: string }> {
+    return this.http.post<{ token: string }>(this.loginUrl, user);
   }
 }
